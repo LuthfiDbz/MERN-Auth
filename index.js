5b95633eec683e65626e6412e6e0b539bd31802c
LuthfiDbz <luthfi.noer18@gmail.com>
2023-11-07 10:35:14 +0700

    feat: package cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser'
+import cors from "cors";
 
 import userRouter from './routes/userRoutes.js'
 import { errorHandler, notFound } from './middleware/errorHandling.js';
@@ -13,6 +14,8 @@ connectDB()
 const port = process.env.PORT || 5000
 
 const app = express()
+
+app.use(cors());
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
